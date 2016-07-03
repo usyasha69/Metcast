f97c3bac76ebb2e2da1a77d3a91699b5ce8e2678
Dmitriy Usic <usyasha69@mail.ru>
2016-07-03 17:31:00 +0300

    commit

--- app/src/main/java/com/example/pk/metcast/MainActivity.java
+++ app/src/main/java/com/example/pk/metcast/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import com.example.pk.metcast.adapters.MyFragmentPagerAdapter;
-import com.example.pk.metcast.fragments.WeatherFragment;
 
 import java.util.ArrayList;
 
